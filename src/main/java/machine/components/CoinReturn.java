089e7e999c88c37b75a4c2e8f15c9111d7419647
Guilhermelsnunes <guilhermelsnunes@gmail.com>
2022-05-05 19:23:36 +0100

    second commit!

---
+++ src/main/java/machine/components/CoinReturn.java
@@ -0,0 +1,29 @@
+package machine.components;
+
+import coins.Coin;
+
+import java.util.ArrayList;
+
+
+public class CoinReturn {
+
+    private ArrayList<Coin> coins;
+
+    public CoinReturn(){
+        coins = new ArrayList<>();
+    }
+
+    public void addCoin(Coin coin){
+        this.coins.add(coin);
+    }
+
+    public double getValueOfCoins(){
+        double total = 0.0;
+        for (Coin coin : this.coins){
+            total += coin.getValue();
+        }
+        return total;
+    }
+
+    // missing the function to return change or reject if not enough money or reject wrong coins
+}
