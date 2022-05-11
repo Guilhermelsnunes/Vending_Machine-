5334570a2608211f561e928644a3435321ce4c9a
Guilhermelsnunes <guilhermelsnunes@gmail.com>
2022-05-12 00:05:05 +0100

    up to DrawerTest

--- src/test/java/VendingMachineTest.java
+++ src/test/java/VendingMachineTest.java
@@ -5,9 +5,12 @@ import machine.components.Drawer;
 import machine.components.EnumCode;
 import machine.components.VendingMachine;
 import org.junit.Before;
+import org.junit.Test;
 import products.Sweet;
 
 import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
 
 //import static com.sun.tools.classfile.Attribute.Code;
 
@@ -38,6 +41,39 @@ public class VendingMachineTest {
         coinReturn = new CoinReturn();
         machine = new VendingMachine(drawers, coinReturn);
     }
+
+    @Test
+    public void canAcceptCoins(){
+        machine.addCoin(tenPence);
+        assertEquals(0.10, machine.getCredit(), 0.01);
+    }
+
+    @Test
+    public void cannotAcceptInvalidCoins(){
+        machine.addCoin(onePence);
+        machine.addCoin(twoPence);
+        assertEquals(0.0, machine.getCredit(),0.01);
+        assertEquals(0.03,machine.getCoinReturn().getValueOfCoins(),0.01);
+    }
+
+    @Test
+    public void invalidCoinReturn(){
+        machine.addCoin(onePence);
+        assertEquals(0.01,machine.getCoinReturn().getValueOfCoins(),0.01);
+    }
+
+    @Test
+    public void canBuyProduct(){
+        machine.addCoin(fiftyPence);
+        Sweet product = (Sweet)machine.vend(EnumCode.A1);
+        assertEquals("Choquito", product.getName());
+        assertEquals(0.00, machine.getCredit(),0.01);
+    }
+
+
+
+
+
 
 
 
